a8252044de56e66a52a4d9506e5fe40621a9c7ad
annarankin <anniesqueedle@gmail.com>
2021-02-05 19:40:33 -0500

    jabberscrip

--- src/http/get-login/index.js
+++ src/http/get-login/index.js
@@ -1,15 +1,20 @@
-let arc = require('@architect/functions')
-let data = require('@begin/data')
+let arc = require("@architect/functions");
+let data = require("@begin/data");
 
-async function get (req) {
-  req.session.pageView = req.session.pageView || 0
-  req.session.pageView += 1
+async function get(req) {
+  let count = (req.session.count || 0) + 1;
 
-  let body = `<h1>Page views this session: ${req.session.pageView}</h1>`
+  let body = `<h1>Page views this session: ${count}</h1>`;
   return {
     body,
+    headers: {
+      "content-type": "text/html; charset=utf8",
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+    },
+    session: { count },
     statusCode: 200,
-  }
+  };
 }
 
-exports.handler = arc.http.async(get)
+exports.handler = arc.http.async(get);
